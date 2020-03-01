ced087b9bf2ecd1fa350e38a902566509ec44a2b
sungwon.cha <mygenie3@naver.com>
2020-03-01 18:09:35 +0900

    fix

---
+++ packages/gatsby-plugin-bluewings/src/gatsby-theme-blog/components/post-footer.jsx
@@ -0,0 +1,52 @@
+// https://raw.githubusercontent.com/gatsbyjs/gatsby/master/packages/gatsby-theme-blog/src/components/post-footer.js
+import React from "react"
+import { Link } from "gatsby"
+import { css, Styled, Flex } from "theme-ui"
+
+import Bio from "gatsby-theme-blog/src/components/bio"
+
+const Footer = ({ previous, next }) => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    <Styled.hr />
+    <Bio post={true}/>
+    {(previous || next) && (
+      <Flex
+        as="ul"
+        css={css({
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+          mt: 0,
+          mb: 3
+        })}
+      >
+        <li css={css({
+          mb: 2
+        })}>
+          {previous && (
+            <Styled.a as={Link} to={previous.slug} rel="prev">
+              ← {previous.title}
+            </Styled.a>
+          )}
+        </li>
+        <li css={css({
+          mb: 2
+        })}>
+          {next && (
+            <Styled.a as={Link} to={next.slug} rel="next">
+              {next.title} →
+            </Styled.a>
+          )}
+        </li>
+      </Flex>
+    )}
+  </footer>
+)
+
+export default Footer
